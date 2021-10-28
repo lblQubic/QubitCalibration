cefc577b816adcf9bd4ad826581dea0248a4dde8
Gang Huang <ghuang@lbl.gov>
2021-10-28 07:01:59 -0700

    new calibration on X6Y3

--- X6Y3/wiremap_X6Y3.py
+++ X6Y3/wiremap_X6Y3.py
@@ -35,13 +35,13 @@ lofreq={'Q0.rdrv':lor
 		}
 elemdict={
  		 'Q1.qdrv':2,'Q1.read':10,'Q1.rdrv':2
-		,'Q2.qdrv':2,'Q2.read':10,'Q2.rdrv':2
-		,'Q0.qdrv':2,'Q0.read':10,'Q0.rdrv':2
-		,'Q3.qdrv':2,'Q3.read':10,'Q3.rdrv':2
+		,'Q2.qdrv':3,'Q2.read':10,'Q2.rdrv':3
+		,'Q0.qdrv':4,'Q0.read':10,'Q0.rdrv':4
+		,'Q3.qdrv':5,'Q3.read':10,'Q3.rdrv':5
 		,'Q4.qdrv':0,'Q4.read':8,'Q4.rdrv':0
 		,'Q5.qdrv':1,'Q5.read':9,'Q5.rdrv':1
-		,'Q6.qdrv':2,'Q6.read':10,'Q6.rdrv':2
-		,'Q7.qdrv':2,'Q7.read':10,'Q7.rdrv':2
+		,'Q6.qdrv':6,'Q6.read':10,'Q6.rdrv':6
+		,'Q7.qdrv':7,'Q7.read':10,'Q7.rdrv':7
 		,'M0.mark':12}
 destdict={'Q0.qdrv':2,'Q0.read':4,'Q0.rdrv':0
 		,'Q1.qdrv':2,'Q1.read':5,'Q1.rdrv':0
