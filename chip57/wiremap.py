fbf429f2a7205054a58a386a3d65961456ef2245
ghuang <ghuang@lbl.gov>
2021-12-24 01:03:48 -0800

    chip 57 Q0, Q5, Q6

--- chip57/wiremap.py
+++ chip57/wiremap.py
@@ -1,32 +1,32 @@
-chanmapqubit={'Q0.rdrv':0
-		,'Q1.rdrv':0
-		,'Q2.rdrv':0
-		,'Q3.rdrv':0
-		,'Q4.rdrv':0
-		,'Q5.rdrv':0
-		,'Q6.rdrv':0
-		,'Q7.rdrv':0
-		,'Q5.qdrv':3
-		,'Q4.qdrv':2
-		,'Q3.qdrv':2
-		,'Q0.qdrv':2
-		,'Q1.qdrv':2
-		,'Q2.qdrv':2
-		,'Q6.qdrv':2
-		,'Q7.qdrv':1
-		,'Q0.read':4
-		,'Q1.read':5
-		,'Q2.read':6
-		,'Q3.read':4
-		,'Q4.read':5
-		,'Q5.read':6
-		,'Q6.read':4
-		,'Q7.read':5
-		,'M0.mark':13
-		,'M1.mark':14
-		,'M2.mark':15
-		,'M3.mark':16
-		}
+#chanmapqubit={'Q0.rdrv':0
+#		,'Q1.rdrv':0
+#		,'Q2.rdrv':0
+#		,'Q3.rdrv':0
+#		,'Q4.rdrv':0
+#		,'Q5.rdrv':0
+#		,'Q6.rdrv':0
+#		,'Q7.rdrv':0
+#		,'Q5.qdrv':3
+#		,'Q4.qdrv':2
+#		,'Q3.qdrv':2
+#		,'Q0.qdrv':2
+#		,'Q1.qdrv':2
+#		,'Q2.qdrv':2
+#		,'Q6.qdrv':2
+#		,'Q7.qdrv':1
+#		,'Q0.read':4
+#		,'Q1.read':5
+#		,'Q2.read':6
+#		,'Q3.read':4
+#		,'Q4.read':5
+#		,'Q5.read':6
+#		,'Q6.read':4
+#		,'Q7.read':5
+#		,'M0.mark':13
+#		,'M1.mark':14
+#		,'M2.mark':15
+#		,'M3.mark':16
+#		}
 ttydev={"qdrvlo":{"deviceid":"a4spi_02","hostname":"192.168.1.24","default":{"chan":3,"freq":5.5e9}}
         ,"readlo":{"deviceid":"a4spi_02","hostname":"192.168.1.24","default":{"chan":1,"freq":6.52e9}}
 		,"readvat":{"deviceid":"attn3","hostname":"192.168.1.24","default":0}
@@ -72,30 +72,30 @@ lofreq={'Q0.rdrv':lor
 		,'vna.read':lor
 		,'vna.rdrv':lor
 		}
-dacelementsdest=['Q0.rdrv'
-		,'Q1.rdrv'
-		,'Q2.rdrv'
-		,'Q3.rdrv'
-		,'Q4.rdrv'
-		,'Q5.rdrv'
-		,'Q6.rdrv'
-		,'Q7.rdrv'
-		,'Q0.qdrv'
-		,'Q1.qdrv'
-		,'Q2.qdrv'
-		,'Q3.qdrv'
-		,'Q4.qdrv'
-		,'Q5.qdrv'
-		,'Q6.qdrv'
-		,'Q7.qdrv'
-		]
+#dacelementsdest=['Q0.rdrv'
+#		,'Q1.rdrv'
+#		,'Q2.rdrv'
+#		,'Q3.rdrv'
+#		,'Q4.rdrv'
+#		,'Q5.rdrv'
+#		,'Q6.rdrv'
+#		,'Q7.rdrv'
+#		,'Q0.qdrv'
+#		,'Q1.qdrv'
+#		,'Q2.qdrv'
+#		,'Q3.qdrv'
+#		,'Q4.qdrv'
+#		,'Q5.qdrv'
+#		,'Q6.qdrv'
+#		,'Q7.qdrv'
+#		]
 elemdict={'Q0.qdrv':0,'Q0.read':8,'Q0.rdrv':0
 		,'Q1.qdrv':1,'Q1.read':8,'Q1.rdrv':1
 		,'Q2.qdrv':2,'Q2.read':8,'Q2.rdrv':2
 		,'Q3.qdrv':3,'Q3.read':8,'Q3.rdrv':3
 		,'Q4.qdrv':4,'Q4.read':8,'Q4.rdrv':4
-		,'Q5.qdrv':5,'Q5.read':10,'Q5.rdrv':5
-		,'Q6.qdrv':6,'Q6.read':9,'Q6.rdrv':6
+		,'Q5.qdrv':5,'Q5.read':8,'Q5.rdrv':5
+		,'Q6.qdrv':6,'Q6.read':8,'Q6.rdrv':6
 		,'Q7.qdrv':7,'Q7.read':8,'Q7.rdrv':7
 		,'M0.mark':12}
 destdict={'Q0.qdrv':1,'Q0.read':4,'Q0.rdrv':0
