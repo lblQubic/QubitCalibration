e945370729c2d84ef6897c6a09528c7c59a138d1
ghuang <ghuang@lbl.gov>
2022-01-06 17:39:48 -0800

    adjust

--- chip57/wiremap.py
+++ chip57/wiremap.py
@@ -39,14 +39,14 @@ lofreq={'Q0.rdrv':lor
 		,'M2.mark':0
 		,'M3.mark':0
 		}
-elemdict={'Q0.qdrv':0,'Q0.read':8,'Q0.rdrv':0
-		,'Q1.qdrv':1,'Q1.read':8,'Q1.rdrv':1
-		,'Q2.qdrv':2,'Q2.read':8,'Q2.rdrv':2
-		,'Q3.qdrv':3,'Q3.read':8,'Q3.rdrv':3
-		,'Q4.qdrv':4,'Q4.read':8,'Q4.rdrv':4
-		,'Q5.qdrv':5,'Q5.read':8,'Q5.rdrv':5
-		,'Q6.qdrv':6,'Q6.read':8,'Q6.rdrv':6
-		,'Q7.qdrv':7,'Q7.read':8,'Q7.rdrv':7
+elemdict={'Q0.qdrv':0,'Q0.read':10,'Q0.rdrv':0
+		,'Q1.qdrv':0,'Q1.read':8,'Q1.rdrv':0
+		,'Q2.qdrv':0,'Q2.read':8,'Q2.rdrv':0
+		,'Q3.qdrv':0,'Q3.read':8,'Q3.rdrv':0
+		,'Q4.qdrv':0,'Q4.read':8,'Q4.rdrv':0
+		,'Q5.qdrv':5,'Q5.read':9,'Q5.rdrv':2
+		,'Q6.qdrv':6,'Q6.read':8,'Q6.rdrv':3
+		,'Q7.qdrv':0,'Q7.read':8,'Q7.rdrv':0
 		,'M0.mark':12}
 destdict={'Q0.qdrv':1,'Q0.read':4,'Q0.rdrv':0
 		,'Q1.qdrv':1,'Q1.read':4,'Q1.rdrv':0
